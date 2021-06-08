bccd2e70a5b87867b54c95131f43e3425c8248c9
sarvani01 <vidya.sarvani@gmail.com>
2021-06-08 21:30:31 +0530

    4 files modified

--- src/app/Title.component.ts
+++ src/app/Title.component.ts
@@ -2,7 +2,16 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'title',
-  template: `<h1>Animals and Birds</h1>`,
-  styles: [`h1 {font-family: Lato;}`]
+  template: `
+    <h1>Animals and Birds</h1>
+  `,
+  styles: [
+    `
+      h1 {
+        font-family: Lato;
+        display: inline;
+      }
+    `
+  ]
 })
 export class TitleComponent {}
