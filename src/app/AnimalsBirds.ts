c7000f1be7e9f91e57dd3094dd69434203bfce82
sarvani01 <vidya.sarvani@gmail.com>
2021-06-09 18:04:51 +0530

    1 file added

---
+++ src/app/AnimalsBirds.ts
@@ -0,0 +1,35 @@
+export class AnimasBirds {
+  animals = [
+    {
+      name: 'Lion',
+      imagePath:
+        'https://i.pinimg.com/originals/c0/d6/25/c0d625a7ef5bc800380b76186f4f1e06.jpg'
+    },
+    {
+      name: 'Tiger',
+      imagePath:
+        'https://st.depositphotos.com/2977105/3691/i/600/depositphotos_36916547-stock-photo-tiger-portrait-horizontal.jpg'
+    },
+    {
+      name: 'Elephant',
+      imagePath:
+        'https://www.sciencemag.org/sites/default/files/styles/article_main_large/public/elephant_1280p.jpg?itok=-qxYsWip'
+    }
+  ];
+  birds = [
+    {
+      name: 'Pecock',
+      imagePath:
+        'https://i.pinimg.com/originals/0f/8c/c4/0f8cc4d1102c7d4215b11bfb32d4e055.jpg'
+    },
+    {
+      name: 'Pegion',
+      imagePath:
+        'https://images.unsplash.com/photo-1534695941753-73cf13435eb4?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGlnZW9ufGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80'
+    },
+    {
+      name: 'Eagle',
+      imagePath: 'https://i.ytimg.com/vi/uK3m6e5SZ3s/maxresdefault.jpg'
+    }
+  ];
+}
