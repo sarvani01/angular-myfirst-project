2920d656fec7c48aec2db6cd446d44086c526107
sarvani01 <vidya.sarvani@gmail.com>
2021-06-09 19:32:35 +0530

    4 files modified

--- src/app/dropdowns/dropdowns.component.ts
+++ src/app/dropdowns/dropdowns.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Output,EventEmitter } from '@angular/core';
 import { AnimalsBirds } from '../AnimalsBirds';
 
 @Component({
@@ -10,11 +10,15 @@ export class DropdownsComponent implements OnInit {
   animalsBirds: AnimalsBirds = new AnimalsBirds();
   animals = [];
   birds = [];
-
+  path:string ='';
+ @Output() animalLink = new EventEmitter<string>();
   constructor() {}
 
   ngOnInit() {
     this.animals = this.animalsBirds.animals;
     this.birds = this.animalsBirds.birds;
+  }
+  onSelectAnimal(){
+    this.animalLink.emit(this.path);
   }
 }
